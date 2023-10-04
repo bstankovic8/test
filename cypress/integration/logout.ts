cbdb479c3b6690d9e68a8e9917144db9b8c0b60c
Bora <bstankovic@knowit.rs>
2023-10-04 13:26:34 +0200

    unapredio dva testa

--- cypress/integration/logout.ts
+++ cypress/integration/logout.ts
@@ -1,19 +1,25 @@
+import loginElements from "cypress/support/commands/loginElements";
+import logoutElements from "cypress/support/commands/logoutElements";
+
+
+
 describe('create', () => {
     it('home page', () => {
         cy.visit('http://localhost:3000/');
     })
   
     it('login', () => {
-      cy.get('[data-cy="login-menu"').click();
-      cy.get('[data-cy="login-email"]').click().type('opaaa@gmail.com');
-      cy.get('[data-cy="login-password"]').click().type('takoe');
-      cy.get('[data-cy="login"]').click();
+      cy.get(loginElements.loginMenu).click();
+      cy.get(loginElements.loginEmail).click().type('opaaa@gmail.com');
+      cy.get(loginElements.loginPassword).click().type('takoe');
+      cy.get(loginElements.loginButton).click();
       cy.contains('User is logged in').should('be.visible');
+      //ovo bi trebalo da se vidi pull noibranch
     })
 
     it('logout', () => {
-      cy.get('[data-cy="logged-user"]').click();
-      cy.contains('Log out').click();
+      cy.get(logoutElements.logoutMenu).click();
+      cy.get(logoutElements.logoutB).click();
       cy.contains('Log in').should('be.visible');
     })
   })
