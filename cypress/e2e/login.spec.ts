a4aa3bec70f42bb037411139c3efb48ce6ba53f0
Bora <bstankovic@knowit.rs>
2024-01-05 17:16:38 +0100

    Ovo sam poceo ovako, ubaciti cy.intercept

--- cypress/e2e/login.spec.ts
+++ cypress/e2e/login.spec.ts
@@ -1,16 +1,1 @@
 import Auth from "cypress/support/classes/auth";
-
-const auth = new Auth();
-
-describe('create', () => {
-  beforeEach(() => {
-      cy.visit('http://localhost:3000/');
-      auth.login('opaaa@gmail.com','takoe');
-  })
-
-  it('login', () => {
-      
-    auth.login('opaaa@gmail.com','takoe');
-    auth.assertMessage('User is logged in')
-  })
-})
--- cypress/e2e/logout.spec.ts
+++ cypress/e2e/logout.spec.ts
@@ -1,19 +1,2 @@
-import { should } from "chai";
 import Auth from "cypress/support/classes/auth";
 import Out from "cypress/support/classes/logout";
-
-const out = new Out();
-const auth = new Auth();
-
-describe('logout', () => {
-    beforeEach(() => {
-        cy.visit('http://localhost:3000/');
-        auth.login('opaaa@gmail.com','takoe');
-        cy.get('[data-cy=logged-user]').should('be.visible');
-    })
-  
-    it ('should logout', () => {
-        out.logout();
-    })
-
-})
